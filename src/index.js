b70be12af346e3b575a461d581a505a34a17ad8a
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-03-20 12:26:03 -0300

    Add Docker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
-const transactionsRoutes = require("./routes/transactions");
+const transactionsRoutes = require("../src/routes/transactions");
 
 //DB
 
