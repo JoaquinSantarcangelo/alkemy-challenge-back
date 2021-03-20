49e96a37afbc06d99c89c67a1e5852194ec0fb16
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-03-20 11:18:06 -0300

    Index structure

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+require("dotenv").config();
+
+const transactionsRoutes = require("./routes/transactions");
+
+//DB
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use("/api/transactions", transactionsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
