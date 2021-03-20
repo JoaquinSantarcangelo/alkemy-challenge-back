00942540d06819e98b64bfc3e9189dc2a37e4a56
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-03-20 11:41:47 -0300

    Transaction Routes

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,7 +1,46 @@
 const express = require("express");
 
+const transactions = [
+  {
+    desc: "Monotributo",
+    amount: 255.2,
+    type: "outbound",
+    category: "taxes",
+    date: new Date(),
+  },
+  {
+    desc: "Impuesto a las ganancias",
+    amount: 55.52,
+    type: "inbound",
+    category: "taxes",
+    date: new Date(),
+  },
+  {
+    desc: "Juego en Steam",
+    amount: 1255.2,
+    type: "outbound",
+    category: "fun",
+    date: new Date(),
+  },
+  {
+    desc: "Mc Donalds Combo Triple Queso",
+    amount: 555,
+    type: "outbound",
+    category: "food",
+    date: new Date(),
+  },
+  {
+    desc: "Ibuprofeno 400 2mg",
+    amount: 155,
+    type: "outbound",
+    category: "health",
+    date: new Date(),
+  },
+];
+
 const getAllTransactions = async (req, res) => {
   try {
+    res.json(transactions);
   } catch (err) {
     res.status(400).json(`Error: ${err}`);
   }
