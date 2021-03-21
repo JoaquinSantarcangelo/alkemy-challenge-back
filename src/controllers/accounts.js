a006cfa47702e67f3ca7c8f5c5c7156bfa405de6
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-03-21 13:38:12 -0300

    Login Structure

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -1,11 +1,10 @@
 const express = require("express");
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-  } catch (err) {
-    res.status(400).json(`Error: ${err}`);
-  }
+  console.log("Handleing Log in");
+  const { email, pw } = req.body;
+
+  console.log(email, pw);
 };
 
 const register = async (req, res) => {
