1426b53805f026fc962d3234131ffcc260a3f209
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-03-21 09:50:44 -0300

    Installed dependencies for Authentication

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE api;
+
 CREATE TABLE users(
     name VARCHAR(30),
     email VARCHAR(40),
     password VARCHAR(10),
     transactions TEXT,
 );
+
 CREATE TABLE transactions(
-    ID INT,
+    id SERIAL PRIMARY KEY,
     desc VARCHAR(24),
     category varchar(30),
     category varchar(30),
