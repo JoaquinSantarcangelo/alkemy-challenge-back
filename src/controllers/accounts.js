8c98c9bd14b68df4598531cd505c5215e3c75d8b
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-03-21 14:00:42 -0300

    Added Register Structure

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -4,15 +4,16 @@ const login = async (req, res) => {
   console.log("Handleing Log in");
   const { email, pw } = req.body;
 
+  res.send("Ok");
+
   console.log(email, pw);
 };
 
 const register = async (req, res) => {
-  const { email, password, firstName } = req.body;
-  try {
-  } catch (err) {
-    res.status(400).json(`Error: ${err}`);
-  }
+  const { email, pw, name } = req.body;
+  console.log("Handleing Register");
+
+  console.log(email, pw, name);
 };
 
 module.exports = {
