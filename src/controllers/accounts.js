a3b55bf6c1b902603544914db29250e302656a77
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-03-26 12:22:50 -0300

    Check for existing account register

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -3,28 +3,53 @@ const express = require("express");
 const login = async (req, res) => {
   console.log("Handleing Log in");
   const { email, pw } = req.body;
-
-  res.send("Ok");
-
   console.log(email, pw);
+
+  conn.query(
+    "SELECT email FROM users WHERE email = ? AND password = ?",
+    [email, pw],
+    (err, user) => {
+      if (err) {
+        res.json({ err });
+      } else {
+        console.log(user);
+        res.send("Added Successfully");
+      }
+    }
+  );
 };
 
 const register = async (req, res) => {
   const { email, password, name } = req.body;
-  console.log(email, password, name);
   console.log("Handleing Register");
 
   const data = { email, password, name };
 
   req.getConnection((err, conn) => {
-    conn.query("INSERT INTO users set ?", data, (err, user) => {
-      if (err) {
-        res.err(err)
-      } else {
-        console.log(user);
-        res.send("Added Successfully");
+    conn.query(
+      "SELECT email FROM users WHERE email = ?",
+      email,
+      (err, user) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (user.length <= 0) {
+            console.log("No hay un usuario registrado");
+            conn.query("INSERT INTO users set ?", data, (err, user) => {
+              if (err) {
+                res.json({ err });
+              } else {
+                console.log(user);
+                res.status(200).send("Usuario Registrado");
+              }
+            });
+          } else {
+            console.log("Ya hay un usuario registrado");
+            res.status(500).send("Ya hay un usuario registrado");
+          }
+        }
       }
-    });
+    );
   });
 };
 
